1fae47f238bc7bdd0961313eda495ad40305e6ed
Roy Adams <roy@eden-grafix.co.uk>
2018-02-08 16:12:58 +0000

    starting to update games routes

--- server/routes/games.js
+++ server/routes/games.js
@@ -7,36 +7,37 @@ var async = require('async');
 require('dotenv').config();
 // objName: 'player_perspectives' objName: ''
 var objName = ['genres', 'player_perspectives', 'game_modes', 'themes'];
-router.get('/getfields', function (req, res, next) {
+router.get('/getfields', function(req, res, next) {
   // create request objects to loop through
   var requests = [{
-    url:'https://igdbcom-internet-game-database-v1.p.mashape.com/genres/?fields=name,id',
-    headers: {
-      'X-Mashape-Key': process.env.X_MASHAPE_KEY,
-      'Accept': 'application/json'
-    }
-  },{
-      url:'https://igdbcom-internet-game-database-v1.p.mashape.com/player_perspectives/?fields=name',
+      url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/genres/?fields=name,id',
       headers: {
-        'X-Mashape-Key': process.env.X_MASHAPE_KEY,
+        'user-key': process.env.X_MASHAPE_KEY,
         'Accept': 'application/json'
       }
-    },{
-        url:'https://igdbcom-internet-game-database-v1.p.mashape.com/game_modes/?fields=name',
-        headers: {
-          'X-Mashape-Key': process.env.X_MASHAPE_KEY,
-          'Accept': 'application/json'
-        }
-      },
-      {
-          url:'https://igdbcom-internet-game-database-v1.p.mashape.com/themes/?fields=name',
-          headers: {
-            'X-Mashape-Key': process.env.X_MASHAPE_KEY,
-            'Accept': 'application/json'
-          }
-        }];
+    }, {
+      url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/player_perspectives/?fields=name',
+      headers: {
+        'user-key': process.env.X_MASHAPE_KEY,
+        'Accept': 'application/json'
+      }
+    }, {
+      url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/game_modes/?fields=name',
+      headers: {
+        'user-key': process.env.X_MASHAPE_KEY,
+        'Accept': 'application/json'
+      }
+    },
+    {
+      url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/themes/?fields=name',
+      headers: {
+        'user-key': process.env.X_MASHAPE_KEY,
+        'Accept': 'application/json'
+      }
+    }
+  ];
 
-async.map(requests, function(obj, callback) {
+  async.map(requests, function(obj, callback) {
     // iterator function
     request(obj, function(error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -62,88 +63,96 @@ async.map(requests, function(obj, callback) {
       for (var i = 0; i < results.length; i++) {
 
         // request body is results[i]
-        var newObj = {data: results[i]}
-         var val = newObj.data;
-         newObj[objName[i]] = val;
+        var newObj = {
+          data: results[i]
+        }
+        var val = newObj.data;
+        newObj[objName[i]] = val;
 
         delete newObj.data;
-      console.log(newObj)
+        console.log(newObj)
         f.push(newObj)
       }
       res.json(f);
     }
   });
-// next();
+  // next();
 })
 
 
-router.get('/preview/:limit', function (req, res) {
+router.get('/preview/:limit', function(req, res) {
   let limit = req.params.limit;
   var options = {
-    url:'https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&order=popularity:desc&filter[release_dates.date][gte]='+date+'&limit='+limit,
+    url: 'https://api-2445582011268.apicast.io/games/?fields=*&order=popularity:desc&filter[release_dates.date][gte]=' + date + '&limit=' + limit,
     headers: {
-      'X-Mashape-Key': process.env.X_MASHAPE_KEY,
+      'user-key': process.env.X_MASHAPE_KEY,
       'Accept': 'application/json'
     }
   }
 
-   request.get(options, function (err, response, body) {
+  request.get(options, function(err, response, body) {
 
-     if(err){
-       return res.status(500).json({
-         title: 'An error has occured',
-         error: err
-       })
-     }
-          res.json({data: JSON.parse(body)});
+    if (err) {
+      return res.status(500).json({
+        title: 'An error has occured',
+        error: err
+      })
+    }
+    res.json({
+      data: JSON.parse(body)
+    });
 
-    })
+  })
 
 })
 
-router.get('/getgame/:id', function (req, res) {
+router.get('/getgame/:id', function(req, res) {
   let id = req.params.id;
   var options = {
-    url:'https://igdbcom-internet-game-database-v1.p.mashape.com/games/'+id+'?fields=*',
+    url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/games/' + id + '?fields=*',
     headers: {
-      'X-Mashape-Key': process.env.X_MASHAPE_KEY,
+      'user-key': process.env.X_MASHAPE_KEY,
       'Accept': 'application/json'
     }
   }
 
-request.get(options, function (err, response, body) {
+  request.get(options, function(err, response, body) {
 
-     if(err){
-       return res.status(500).json({
-         title: 'An error has occured',
-         error: err
-       })
-     }
-          res.json({data: JSON.parse(body)});
-    })
+    if (err) {
+      return res.status(500).json({
+        title: 'An error has occured',
+        error: err
+      })
+    }
+    res.json({
+      data: JSON.parse(body)
+    });
+  })
 });
 
-router.get('/search/:query', function (req, res) {
+router.get('/search/:query', function(req, res) {
   let q = req.params.query;
   let limit = req.params.limit;
   let query = encodeURIComponent(q)
 
   var options = {
-    url:'https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search='+query+'&fields=*',
+    url: 'https://igdbcom-internet-game-database-v1.p.mashape.com/games/?search=' + query + '&fields=*',
     headers: {
-      'X-Mashape-Key': process.env.X_MASHAPE_KEY,
+      'user-key': process.env.X_MASHAPE_KEY,
       'Accept': 'application/json'
     }
   }
 
-request.get(options, function (err, response, body) {
-     if(err){
-       return res.status(500).json({
-         title: 'An error has occured',
-         error: err
-       })
-     }
-          res.json({data: JSON.parse(body)});
-    })
+  request.get(options, function(err, response, body) {
+    if (err) {
+      return res.status(500).json({
+        title: 'An error has occured',
+        error: err
+      })
+    }
+    res.json({
+      data: JSON.parse(body)
+    });
+  })
 })
 module.exports = router;
--- src/app/components/games/games.component.ts
+++ src/app/components/games/games.component.ts
@@ -12,46 +12,46 @@ import 'rxjs';
 export class GamesComponent implements OnInit {
   public catTitle = 'Loading Games content';
   public loading: boolean = true;
-  public items:Array<any> = [];
-  public isActive:string = 'inActive';
-  public playTitle:string = 'faded';
-  public showloader:string = 'opaque';
+  public items: Array<any> = [];
+  public isActive: string = 'inActive';
+  public playTitle: string = 'faded';
+  public showloader: string = 'opaque';
   constructor(private gamesService: GamesService,
-              private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute) { }
   ngOnInit() {
     this.activatedRoute.params
-    .flatMap((params:Params)=>{
-      console.log(params)
-      return this.gamesService.getItems(50)
-    })
-    .subscribe((data)=>{
-      if(data){
-  //  console.log(data)
-      // data.json().data.albums.items
-      this.items = data;
-      this.items = this.items.map(data=>{
-          // console.log(data.cover)
-        if(data.cover != undefined){
-            data.cover.cloudinary_id = 'https://images.igdb.com/igdb/image/upload/t_cover_big/'+data.cover.cloudinary_id+'.jpg'
+      .flatMap((params: Params) => {
+        console.log(params)
+        return this.gamesService.getItems(50)
+      })
+      .subscribe((data) => {
+        if (data) {
+          //  console.log(data)
 
-        }else{
-          data.cover = {cloudinary_id: "/assets/image404@2x.png"}
+          this.items = data;
+          this.items = this.items.map(data => {
+            // console.log(data.cover)
+            if (data.cover != undefined) {
+              data.cover.cloudinary_id = 'https://images.igdb.com/igdb/image/upload/t_cover_big/' + data.cover.cloudinary_id + '.jpg'
+
+            } else {
+              data.cover = { cloudinary_id: "/assets/image404@2x.png" }
+            }
+            return data;
+          })
         }
-        return data;
-        })
-      }
-      setTimeout(()=>{
-        this.isActive = 'active';
-        this.loading = false;
-        this.playTitle = 'opaque';
-        this.showloader = 'faded';
-      }, 300)
+        setTimeout(() => {
+          this.isActive = 'active';
+          this.loading = false;
+          this.playTitle = 'opaque';
+          this.showloader = 'faded';
+        }, 300)
 
-    //  console.log(data)
-    },err => {
-      this.loading = false;
-      console.log(err)
-    })
+        //  console.log(data)
+      }, err => {
+        this.loading = false;
+        console.log(err)
+      })
   }
 
 }
